c4df991848599e62b3343f4ce71ec47b9ce1be2f
mahir-shaikh <mahirthebest95@gmail.com>
2021-05-07 16:53:11 +0530

    Updated flow of calls ,peer, socket to avoid any race conditions

--- zoom-clone-client/src/app/views/room/room.component.ts
+++ zoom-clone-client/src/app/views/room/room.component.ts
@@ -24,53 +24,61 @@ export class RoomComponent implements OnInit {
 
   ngOnInit() {
     this.roomId = this.route.snapshot.paramMap.get('roomId');
-    if(!this.roomId){
+    if (!this.roomId) {
       var number = Math.random() // 0.9394456857981651
       number.toString(36); // '0.xtis06h6'
       var id = number.toString(36).substr(2, 9); // 'xtis06h6'
       this.router.navigate(['room', id])
-    }else{
+    } else {
       this.initialize()
     }
   }
-  
-  initialize(){
-    this.peer = new Peer(); 
-    this.peer.on('open', (id: any)=>{
-      this.socketService.connect(this.roomId, id)
 
-      this.socketService.getSocket()?.on('user-disconnected', (userId: any)=>{
-        console.log('user dis-connected: ', userId)
-        this.peers?.userId?.close();
-        document.getElementById(userId)?.remove()
-      })
-    })
+  initialize() {
+    navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 300 }, audio: true }).then((stream) => {
+      this.peer = new Peer();
+      // Put everything in callback to avoid any race conditions
+      this.peer.on('open', (id: any) => {
+        // Once peer connection is done, connect to socket(room)
+        this.socketService.connect(this.roomId, id)
 
-    const myVideo = document.createElement('video')
-    myVideo.muted = true;
+        //My Video Element
+        const myVideo = document.createElement('video')
+        myVideo.id = "myVideo"
+        myVideo.muted = true;
+        // add my video screen to view
+        this.addVideoStream(myVideo, stream)
 
-    navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 300 }, audio: true }).then((stream)=>{
-      this.addVideoStream(myVideo, stream)
-      
-      this.peer.on('call', (call: any)=>{
-        call.answer(stream)
+        // Listen to other people joinging the room
+        this.socketService.getSocket()?.on('user-connected', (userId: any) => {
+          console.log('New user connected: ', userId)
+          setTimeout(() => {
+            // this function will basically call the other peer(which was recently connected)
+            this.connectToNewUser(userId, stream)
+          }, 1000);
+        })
+
+        // Listen to other people joining the peer
+        this.peer.on('call', (call: any) => {
+          // Answering the call will send our data to the person calling
+          call.answer(stream)
 
 
-        const video = document.createElement('video')
-        video.id = "myVideo"
-        call.on('stream', (userVideoStream: MediaStream)=>{
-          this.addVideoStream(video, userVideoStream)
+          // On recieving their stream, add video to view
+          const video = document.createElement('video')
+          call.on('stream', (userVideoStream: MediaStream) => {
+            this.addVideoStream(video, userVideoStream)
+          })
+        })
+
+        // When user leaves the room, remove their video from view
+        this.socketService.getSocket()?.on('user-disconnected', (userId: any) => {
+          console.log('user dis-connected: ', userId)
+          this.peers?.userId?.close();
+          document.getElementById(userId)?.remove()
         })
       })
-
-      this.socketService.getSocket()?.on('user-connected', (userId: any)=>{
-        console.log('New user connected: ', userId)
-        setTimeout(() => {
-          this.connectToNewUser(userId, stream)
-        }, 1000);
-      })
-
-    }).catch((err)=>{
+    }).catch((err) => {
       console.error('Failed to get local stream', err);
     })
   }
@@ -83,15 +91,15 @@ export class RoomComponent implements OnInit {
     this.VideoGridRef?.nativeElement.append(video)
   }
 
-  connectToNewUser(userId: string, stream: MediaStream){
+  connectToNewUser(userId: string, stream: MediaStream) {
     const call = this.peer.call(userId, stream)
     const video = document.createElement('video')
     video.id = userId;
-    call.on('stream', (userVideoStream: MediaStream)=>{
+    call.on('stream', (userVideoStream: MediaStream) => {
       this.addVideoStream(video, userVideoStream)
     })
 
-    call.on('close', ()=>{
+    call.on('close', () => {
       video.remove()
     })
 
